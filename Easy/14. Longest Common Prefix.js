5ba72e937bdccb083361693e487ddba3b7b19772
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2018-12-28 16:06:46 -0800

    Updated challenge 14

--- Easy/14. Longest Common Prefix.js
+++ Easy/14. Longest Common Prefix.js
@@ -15,10 +15,39 @@ Input: ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
 
+
 /**
  * @param {string[]} strs
  * @return {string}
 
+   SMARTER:
+    1. Set prefix to 1st element of strs: it is the max prefix we can have
+    2. Loop through str and for each element, 
+            -> while it doesn't contains the current prefix
+            -> remove last element of prefix
+    3. At the end, we will have the prefix that is contained in all elements of strs
+
+    Time: O(S), where S is the sum of all characters in all strings
+    Space: O(1)
+
+ */
+var longestCommonPrefix = function(strs) {
+    if(!strs || strs.length <= 0) return "";
+    let prefix = strs[0];
+    for(let x of strs) {
+        while(x.indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if(prefix.length <= 0) return "";
+        }
+    }
+    return prefix;
+};
+    
+
+/*************************************************************/
+
+
+/**
     1. Take 1st element of strs and map all it's post prefixes to hash table
     2. Loop through strs
     3. For each element, check all possible prefixes and verify if they are in hash table
@@ -31,7 +60,6 @@ var longestCommonPrefix = function(strs) {
     if(!strs || strs.length <= 0) return "";
     let prefix = "", maxPrefixLocal = "", minPrefix = strs[0];
     let hash = {};
-    let count = 0; //counts number of common prefixes
     for(let i of strs[0]) {
         prefix += i;
         hash[prefix] = 1;
@@ -45,6 +73,8 @@ var longestCommonPrefix = function(strs) {
             if(typeof hash[prefix] !== 'undefined') {
                 maxPrefixLocal = prefix;
                 hasCommonPrefix = true;
+            } else {
+                break;      //no point to continue if there are no more prefix after
             }
         }
         if(!hasCommonPrefix) return "";     //means that at least one string has no common prefix
