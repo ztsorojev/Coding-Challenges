fe9d01467eed5352e0183973313ffa11684be102
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2019-01-08 10:43:56 -0800

    Add BST challenge

---
+++ BST/98. Validate Binary Search Tree.js
@@ -0,0 +1,63 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+- The left subtree of a node contains only nodes with keys less than the node's key.
+- The right subtree of a node contains only nodes with keys greater than the node's key.
+- Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input:
+    2
+   / \
+  1   3
+Output: true
+
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+
+   ALGO: use recursion
+
+    - Basically, for each node, we will keep track of the range it must be in
+    - the min value will be the minimum it can be
+    - the max value will be it's maximum
+    - We will use a helper function to go over the whole BST and keep track of min and max
+
+
+ */
+var isValidBST = function(root) {
+    return isValidBSTsub(root, null, null);
+};
+
+//val will be value of original root node of BST
+var isValidBSTsub = function(node, min, max) {
+    if(node === null) return true;
+    if(min !== null && node.val <= min) return false;
+    if(max !== null && node.val >= max) return false;
+    
+    const leftValid = (node.left !== null)? isValidBSTsub(node.left, min, node.val) : true;
+    const rightValid = (node.right !== null)? isValidBSTsub(node.right, node.val, max) : true;
+
+    return leftValid && rightValid;
+}
