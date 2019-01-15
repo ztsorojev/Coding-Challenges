538428466d64b253256548ef86b5e3b0eabc4d97
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2019-01-14 20:31:02 -0800

    Add to Theory/ BinarySearch

---
+++ THEORY/Java/Algorithms/BinarySearch.java
@@ -0,0 +1,17 @@
+int binarySearch(int[] arr, int x) {
+	int left = 0;
+	int right = arr.length() - 1;
+
+	while(left<=right) {
+		int mid = (left+right)/2;
+
+		if(arr[mid] == x) return mid;
+		else if(arr[mid] > x) {
+			right = mid - 1;
+		} else {
+			left = mid + 1;
+		}
+	}
+
+	return -1;
+}
