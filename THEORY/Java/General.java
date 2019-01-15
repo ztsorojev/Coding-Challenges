a6ff420a4476cd2b43e9ff601f758f0b75b7c61b
Zaurbek Tsorojev <tsorojev.zaurbek@gmail.com>
2019-01-15 09:40:37 -0800

    Add challenge TwoSum in Java

--- THEORY/Java/General.java
+++ THEORY/Java/General.java
@@ -1,7 +1,26 @@
-//ARRAYS
+/****************
+ * ARRAYS
+ */
+
 int[] num = new int[5]; 	//create empty array of 5 elements
+int[] myIntArray = {1,2,3}; //create array initialized with [1,2,3]
 
 int[][] num = new int[5][2];
+int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+
+//To assign values, you use CURLY BRACKETS
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+//Loop through array
+for (String i : cars) {
+  System.out.println(i);
+}
 
 
 Math.random()		//returns random float between 0 and 1 (not included)
+
+
+//LENGTH
+.length 	//can be used for int[], double[], String[] to know the length of the ARRAYS!
+
+.length() 	//can be used for String, StringBuilder, etc 
